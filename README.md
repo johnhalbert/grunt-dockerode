9401608bc6435fbc8d7836d578826668bc8ff2bb
John Halbert <johnhalbert@gmail.com>
2017-12-01 20:01:47 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,237 @@
+# grunt-dockerode
+
+> Grunt plugin providing dockerode support.
+
+## Getting Started
+This plugin requires Grunt `~0.4.5` and NodeJS `>= 6.12.0`.
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-dockerode --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-dockerode');
+```
+
+## The "dockerode" task
+
+### Overview
+In your project's Gruntfile, add a section named `dockerode` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  dockerode: {
+    cmd: '', // Docker command
+    options: {
+      // Configures connection to Docker
+    },
+    opts: {
+      // Passed in request to Docker
+    }
+  },
+});
+```
+
+### Options & opts
+
+Options are used to specify how to connect to the docker daemon/server, opts are passed to the docker daemon/server when making a request. See [dockerode](https://www.npmjs.com/package/dockerode) documentation for more information on what options can be passed.
+
+Task and target specific options don't mix well.  Generally prefer to use one over the other, else results may not be what you expected.
+
+Opts and their possible combinations can be found for a given command in the [Docker API documentation](https://docs.docker.com/engine/api/version-history/).
+
+### Command-specific options
+
+Some commands have options specified outside the opts object.  Generally these originate from `dockerode` itself, and represent arguments not passed to Docker but used to change the behavior of `dockerode` when interacting with Docker.  For this reason they exist at the same level as `opts` and `options` in the task's configuration.  For more information on the origin properties that exist outside options and opts, you can refer to [dockerode source](https://github.com/apocas/dockerode) for a specific command you are running.
+
+Command examples below will contain all additional configuration properties that can exist outside the opts and options configuration.
+
+### Usage Examples - run
+
+```js
+{
+  command: 'run',                                  // Docker command
+  image: 'docker/whalesay',                        // Image to run
+  cmd: [ 'cowsay', 'grunt-dockerode is awesome!' ] // Passed to Docker
+}
+```
+
+### Usage Examples - pull
+
+```js
+{
+  command: 'pull',
+  repoTag: 'docker/whalesay' // Repo to pull from 
+}
+```
+
+### Usage Examples - push
+
+```js
+{
+  command: 'push',
+  name: 'repo/image:tag' // Where to push
+  opts: {
+    tag: 'tag',
+    // To have dockerode base64 encode your auth information
+    authconfig: {
+      username: '',
+      password: '',
+      auth: '',
+      email: '',
+      serveraddress: ''
+    },
+    // Or if you already have a base64 encoded auth config
+    authconfig: {
+      key: '<base64 key>' 
+    }
+  }
+}
+```
+
+### Usage Examples - build
+
+Building requires a `context` and `src` be provided.  Additional opts can be provided per the Docker API.  `dockerode` looks for a `Dockerfile` at the root of the directory structure provided by the `src` option.  If this is not the case, a custom dockerfile location can be passed to opts, relative to the root of the src directories.
+
+```js
+{
+  command: 'build',
+  context: __dirname,
+  src: [ './**' ],
+  opts: {
+    t: 'tagname',
+    dockerfile: 'customfile'
+  }
+}
+```
+
+### Usage Examples - stop 
+
+```js
+{
+  command: 'stop',
+  id: 'container_name_or_id'
+}
+```
+
+### Usage Examples - start 
+
+```js
+{
+  command: 'start',
+  id: 'container_name_or_id'
+}
+```
+
+### Usage Examples - kill 
+
+```js
+{
+  command: 'kill',
+  id: 'container_name_or_id'
+}
+```
+
+### Usage Examples - ps 
+
+The `ps` task allows for more flexibility than the docker cli's ps command.  Most importantly, in addition to being able to have more control over details retrieved about containers, it allows for a custom mapping of information.  Data is displayed in columns, provided by [columnify](https://www.npmjs.com/package/columnify).  
+
+In addition to the normal opts, the `ps` task will take a `cols` object, which provides as keys the columns to display.  Key values can either be `true`, to simply display the value, or a function to perform transformations on the information such as formatting it for display.  Additionally, a `colOpts` object can be passed, which will be provided as the second argument to [columnify](https://www.npmjs.com/package/columnify), providing additional control over the output.
+
+```js
+{
+  command: 'ps',
+  opts: {
+    all: true
+  },
+  cols: {
+    Id: true,
+    Names: names => names.map(n => n.substr(1)).join(', ')
+  }
+  colOpts: {
+    Id: { maxWidh: 20 },
+    Names: { align: 'right' }
+  }
+}
+```
+
+### Usage Examples - rm
+
+```js
+{
+  command: 'rm',
+  id: 'container_name_or_id'
+}
+```
+
+### Usage Examples - inspect 
+
+```js
+{
+  command: 'inspect',
+  id: 'container_name_or_id'
+}
+```
+
+### Usage Examples - exec 
+
+Executes command in given container.  Notable here, the command is commonly used as such with the docker cli: `docker exec -it <container> <command>`.  To reproduce this, the opts in the example configuration below should be used.
+
+```js
+{
+  command: 'exec',
+  id: 'container_name_or_id',
+  opts: {
+    AttachStdin: true,
+    AttachStdout: true,
+    AttachStderr: true,
+    Tty: true,
+    Cmd: ['bash']
+  }
+}
+```
+
+### Usage Examples - restart
+
+```js
+{
+  command: 'restart',
+  id: 'container_name_or_id'
+}
+```
+
+### Usage Examples - logs
+
+`opts` must contain either `stdout` or `stderr` members set to `true`, else there will be nothing to log.
+
+```js
+{
+  command: 'logs',
+  id: 'container_name_or_id',
+  opts: {
+    follow: true,
+    stdout: true,
+    stderr: true
+  }
+}
+```
+
+### Usage Examples - tag
+
+```js
+{
+  command: 'tag', 
+  name: 'my_image:latest',
+  opts: {
+    repo: 'private-repo.tld/my_image',
+    tag: 'latest'
+  }
+}
+```
+
+## Release History
+_(Nothing yet)_
---
+++ tasks/dockerode.js
@@ -0,0 +1,242 @@
+/*
+ * grunt-dockerode
+ * https://github.com/johnhalbert/grunt-dockerode
+ *
+ * Copyright (c) 2017 John Halbert
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+const Docker      = require('dockerode'),
+      Promise     = require('bluebird'),
+      cliSpinners = require('cli-spinners'),
+      readline    = require('readline'),
+      fs          = require('fs'),
+      columnify   = require('columnify'),
+      JSONStream  = require('JSONStream');
+
+module.exports = function(grunt) {
+  grunt.registerMultiTask('dockerode', 'Grunt plugin providing dockerode support.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    const options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    const src = this.files.reduce((all, files) => {
+      const src = files.src.filter(fd => {
+        const stats = fs.statSync(fd);
+        return !stats.isDirectory();
+      });
+
+      return all.concat(src);
+    }, []);
+
+    const data = this.data;
+    data.src = src;
+
+    let spinnerInterval, spinnerFrame = 0, curTask = '', numLines; 
+    const done = this.async();
+
+    if (this.data.command && isDockerCommand(this.data.command)) {
+      runDockerCommand(this.data.cmd, data, options)
+        .catch(err => grunt.log.error(err.message));
+    }
+    else if (!this.data.command)
+      throw new Error(`No Docker command supplied!`);
+    else 
+      throw new Error(`${this.data.command} is not a valid Docker command`);
+
+    /**
+     * Returns true/false whether given command is valid docker command
+     * supported by plugin
+     * @param {String} cmd The command to evaluate
+     * @return {Boolean} true = valid command, false = not
+     */
+    function isDockerCommand(cmd) {
+      const commands = [ 'run', 'pull', 'push', 'build', 'stop', 'start', 'kill', 
+        'ps', 'rm', 'inspect', 'exec', 'restart', 'logs', 'stats', 'tag' ];
+
+      return ~commands.indexOf(cmd);
+    }
+
+    /**
+     * Runs given docker command
+     * @param {String} cmd The command to run
+     * @param {Object} args The arguments to pass to the dockerode method
+     * @param {Object} opts The options to pass to the docker server
+     * @return {Promise<Any>} Promise which resolves with variadic type
+     */
+    function runDockerCommand(cmd, args, opts) {
+      const docker = new Docker(opts);
+
+      switch (cmd) {
+        case 'run':
+          if (!args.stream)
+            args.stream = process.stdout;
+          return docker.run(args.image, args.cmd, args.stream, args.create_options, args.start_options)
+            .then(done);
+        case 'pull':
+          curTask = `Pulling ${args.repoTag}`;
+          return docker.pull(args.repoTag, args.opts)
+            .then(handleReadStream);
+        case 'push':
+          curTask = `Pushing ${args.name}`;
+          return docker.getImage(args.name)
+            .push(args.opts, undefined, args.auth)
+            .then(handleReadStream);  
+        case 'tag':
+          return docker.getImage(args.name)
+            .tag(args.opts)
+            .then(() => {
+              grunt.log.ok(`Tagging ${args.opts.repo}:${args.opts.tag}: success!`);
+              done();
+            });
+        case 'build':
+          curTask = `Building ${args.opts && args.opts.t ? args.opts.t : 'docker image'}`;
+          const { context, src } = args;
+          return docker.buildImage({ context, src }, args.opts)
+            .then(handleReadStream);
+        case 'ps':
+          return docker.listContainers(args.opts)
+            .then(containers => {
+              // If cols was passed in task object, map the docker server
+              // response to include only selected columns and to run any
+              // transformations provided by transform functions
+              if (args.cols) {
+                containers = containers.map(cntr => {
+                  const container = {};
+                  for (let col in args.cols) {
+                    if (args.cols[col] instanceof Function)
+                      container[col] = args.cols[col](cntr[col]);
+                    else
+                      container[col] = cntr[col];
+                  }
+                  return container;
+                });
+              }
+
+              // Logs the output from columnify. colOpts can be passed in
+              // the task object to configure columnify output further.
+              grunt.log.writeln(columnify(containers, args.colOpts));
+              done();
+            });
+        case 'stop':
+        case 'start':
+        case 'kill':
+        case 'restart':
+          return docker.getContainer(args.id)[cmd](args.opts)
+            .then(result => {
+              grunt.log.ok(result.output || 'Success!');
+              done();
+            });
+        case 'rm':
+          // rm is not a function
+          return;
+        case 'inspect':
+          return docker.getContainer(args.id)[cmd](args.opts)
+            .then(result => {
+              grunt.log.writeln(JSON.stringify(result, null, 2));
+              done();
+            });
+        case 'exec':
+          return docker.getContainer(args.id)[cmd](args.opts)
+            .then(exec => exec.start({ stdin: args.opts.AttachStdin, ...args.opts }))
+            .then(exec => {
+              const stream = exec.output;
+              stream.on('end', () => done());
+              if (args.opts) {
+                if (args.opts.AttachStdin) 
+                  process.stdin.pipe(stream);
+                if (args.opts.AttachStdout || args.opts.AttachStderr) 
+                  stream.pipe(process.stdout);
+              }
+            });
+        case 'logs':
+          return docker.getContainer(args.id)[cmd](args.opts)
+            .then(stream => {
+              stream.on('end', () => done());
+              stream.pipe(process.stdout);
+             });
+        case 'stats':
+          // There's a lot here and not quite sure that this fits well with
+          // grunt paradygm.  Would be better off using other plugins that can
+          // just spawn a new process with the docker cli to get these stats
+          // than trying to fit them in to some custom implementation...
+          // Leaving alone for now, will not document.
+          return docker.getContainer(args.id)[cmd](args.opts)
+            .then(stream => {
+              stream.on('data', data => {
+                const dataObj = JSON.parse(data);
+                readline.cursorTo(process.stdout, 0);
+                readline.moveCursor(process.stdout, 0, -(numLines+1));
+                readline.clearScreenDown(process.stdout);
+                numLines = Object.keys(dataObj).length;
+                grunt.log.writeln(columnify(dataObj));
+              });
+            });
+      }
+    }
+
+    /**
+     * Handles reading stream, displaying busy animation and parsing errors
+     * @param {ReadStream} stream ReadStream returned from docker modem
+     * @return {void}
+     */
+    function handleReadStream(stream) {
+      // Errors can still appear in the response from the docker server even
+      // though the response code suggests success. Stream needs to be parsed
+      // in order to look for error messages.
+      const errorStream = JSONStream.parse('error')
+      errorStream.on('data', errMsg => {
+        readline.cursorTo(process.stdout, 0);
+        process.stdout.write('');
+        throw new Error(errMsg);
+      });
+      stream.pipe(errorStream);
+      
+      // Shows animation while the stream is being processed
+      startCliSpinner();
+
+      // If no error messages have appeared by the time the stream ends
+      // we can get rid of the spinner and display a success message.
+      stream.on('end', function() {
+        clearCliSpinner();
+        readline.cursorTo(process.stdout, 0);
+        grunt.log.ok(`${curTask}: success!`);
+        done();
+      });
+    }
+
+    /**
+     * Sets an interval that updates the busy animation on the consol
+     * @return {void}
+     */
+    function startCliSpinner() {
+      // Produces busy animation by resetting the last line of stdout and
+      // updating it with new frames from the animation.
+      spinnerInterval = setInterval(() => {
+        readline.cursorTo(process.stdout, 0);
+        process.stdout.write(
+          `${curTask}.. ${cliSpinners.bouncingBall.frames[spinnerFrame]}`
+        );
+        
+        // Increments spinner frames
+        if (spinnerFrame === cliSpinners.bouncingBall.frames.length-1)
+          spinnerFrame = 0;
+        else
+          spinnerFrame++;
+      }, 80);
+    }
+
+    /**
+     * Wrapper around clearInterval that passes the spinner interval set by
+     * startCliSpinner function
+     * @return {void}
+     */
+    function clearCliSpinner() {
+      clearInterval(spinnerInterval);
+    }
+  });
+};
